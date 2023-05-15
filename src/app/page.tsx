06be1e07613fcb4cc35929ba7c524071de518a70
Fynises <fynises.dev@proton.me>
2023-05-15 18:57:55 +0100

    implemented homepage and imported mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,114 +1,124 @@
+'use client';
 import * as React from 'react';
-import Image from 'next/image';
+import { Box, Container } from '@mui/system';
+import { Card, CardActionArea, CardContent, CardHeader, Typography } from '@mui/material';
+
+const secondaryColor = '#006342';
+const topBackgroundColor = '#004A31';
 
 export default function Home() {
+
+  const handleClick = () => {
+    console.log('todo: handleClick');
+  };
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-        <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-          Get started by editing&nbsp;
-          <code className='font-mono font-bold'>src/app/page.tsx</code>
-        </p>
-        <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
-          <a
-            className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
-            href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            By{' '}
-            <Image
-              src='/vercel.svg'
-              alt='Vercel Logo'
-              className='dark:invert'
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <Box sx={{
+        height: 460,
+        backgroundColor: topBackgroundColor
+      }}>
+        <Container maxWidth='lg' sx={{ paddingTop: 4 }}>
+          <Box sx={{
+            display: 'flex',
+            height: 72,
+            backgroundColor: secondaryColor,
+            borderRadius: '24px',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white'
+          }}>
+            <Box>
+              <Typography>info bar placeholder</Typography>
+            </Box>
+          </Box>
+          <Box sx={{ width: '40%' }}>
+            <Typography variant='h2' color='white' sx={{ paddingTop: 2 }}>
+              The open source twitch streaming suite
+            </Typography>
+            <Card elevation={0} sx={{
+              display: 'flex',
+              backgroundColor: secondaryColor,
+              margin: 3,
+              width: 256,
+              height: 48,
+              color: 'white',
+              borderRadius: '24px',
+              alignItems: 'center',
+              justifyContent: 'center',
+              border: 1,
+              borderColor: '#00B075'
+            }}>
+              <CardActionArea onClick={handleClick}>
+                <CardContent>
+                  <Typography variant='body1'>
+                    Sign in with twitch
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Box>
+        </Container>
+      </Box>
+      <FeaturesSection />
+      <PageFooter />
+    </>
+  );
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-          src='/next.svg'
-          alt='Next.js Logo'
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+function FeaturesSection() {
+  return (
+    <Box sx={{ height: 400 }}>
+      <Container maxWidth='lg' sx={{ paddingTop: 3 }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Typography variant='h3' sx={{
+            borderBottom: 4,
+            borderBottomColor: topBackgroundColor,
+            paddingX: 3,
+          }}>
+            Features
+          </Typography>
+        </Box>
+        <Box sx={{
+          display: 'flex',
+          alignContent: 'center',
+          justifyContent: 'space-evenly',
+          marginTop: 3
+        }}>
+          <FeaturesItem title='test1' description='test1' />
+          <FeaturesItem title='test2' description='test2' />
+          <FeaturesItem title='test3' description='test3' />
+        </Box>
+      </Container>
+    </Box>
+  );
+}
 
-      <div className='mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left'>
-        <a
-          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+interface FeaturesItemProps {
+  title: string,
+  description: string,
+}
 
-        <a
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+function FeaturesItem(props: FeaturesItemProps) {
+  return (
+    <Card elevation={0} sx={{
+      width: 224,
+      height: 224,
+      borderRadius: 4,
+      backgroundColor: '#f5f5f5',
+      border: 1,
+      borderColor: topBackgroundColor
+    }}>
+      <CardHeader title={props.title} sx={{ textAlign: 'center' }} />
+      <CardContent>
+        <Typography>{props.description}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
 
-        <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+function PageFooter() {
+  return (
+    <Box sx={{ height: 200, backgroundColor: '#404040' }}></Box>
   );
 }
